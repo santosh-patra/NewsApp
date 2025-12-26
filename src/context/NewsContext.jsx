6e2697dee1afa04ff69fa97415a5fc1d46087e97
Santosh Patra <patrasantoshkl@gmail.com>
2025-12-26 22:50:47 +0530

    initial push

--- src/context/NewsContext.jsx
+++ src/context/NewsContext.jsx
@@ -18,7 +18,7 @@ const NewsContextProvider = ({ children }) => {
             const response = await api.get(`${url}&apiKey=6d89567ccdd24b8b88ed3eb50090e173`)
             // console.log('fetchNewsresponse--->', response.data)
             setLoading(false)
-
+ 
             return response.data
         } catch (error) {
             console.log("Error on FetchNews Function--->", error)
